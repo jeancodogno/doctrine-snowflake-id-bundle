e163b5d59e0233f3a9e72bb7f1aa531b6362316a
Jean Carlo Codogno <jeancarlo.eng.comp@gmail.com>
2025-04-23 02:02:00 -0300

    refactor: adjust code style

--- src/Services/SnowflakeGenerator.php
+++ src/Services/SnowflakeGenerator.php
@@ -5,18 +5,20 @@ declare(strict_types=1);
 namespace JeanCodogno\DoctrineSnowflakeIdBundle\Services;
 
 use Godruoyi\Snowflake\Snowflake;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 class SnowflakeGenerator
 {
     private Snowflake $snowflake;
 
     public function __construct(
-        ParameterBagInterface $params
+        #[Autowire(param: 'snowflake_id.start_timestamp')]
+        ?int $startTimestamp = null,
+        #[Autowire(param: 'snowflake_id.datacenter_id')]
+        int $datacenterId = 0,
+        #[Autowire(param: 'snowflake_id.worker_id')]
+        int $workerId = 0,
     ) {
-        $startTimestamp = $this->getParameter($params, 'start_timestamp');
-        $datacenterId = (int) $this->getParameter($params, 'datacenter_id');
-        $workerId = (int) $this->getParameter($params, 'worker_id');
 
         $this->snowflake = new Snowflake($datacenterId, $workerId);
 
@@ -25,26 +27,8 @@ class SnowflakeGenerator
         }
     }
 
-    public function getParameter(ParameterBagInterface $params, string $key): ?int
-    {
-        $workerId = $params->has('snowflake_id.'.$key)
-        ? $params->get('snowflake_id.'.$key)
-        : 0;
-
-        return $this->ensureInt($key, $workerId);
-    }
-
     public function generate(): string
     {
         return $this->snowflake->id();
-    }
-
-    private function ensureInt(string $key, mixed $value): int
-    {
-        if (! is_int($value)) {
-            throw new \InvalidArgumentException(ucfirst($key) . ' must be an integer.');
-        }
-
-        return $value;
     }
 }
